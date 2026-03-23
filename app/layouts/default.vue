b274051a45bb7101413efdde2370659e83807900
SamMa7 <sam.maass1998@gmail.com>
2026-03-23 15:59:01 +0100

    allow multiple background images

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,14 +2,23 @@
     <div class="min-h-screen flex flex-col">
         <Header />
 
-        <div 
-            class="w-full flex justify-center grow relative"
-            :style="bgImg?.image.url 
-                ? { backgroundImage: `url(${bgImg.image.url})` } 
-                : {}"
-        >
-            <div class="overlay-dark" />
-            <div class="container bg-light z-10 px-14 py-10"> 
+        <div class="w-full flex justify-center grow relative">
+            <ClientOnly>
+                <div 
+                    v-if="bgImgUrls && bgImgUrls.length"
+                    class="absolute inset-0 grid overflow-hidden"
+                    :style="gridStyle"
+                >
+                    <div
+                        v-for="i in totalTiles"
+                        :key="i"
+                        class="w-[196px] h-[277px] bg-cover"
+                        :style="{ backgroundImage: `url(${bgImgUrls[i % bgImgUrls.length]})` }"
+                    />
+                </div>
+                <div class="overlay-dark" />
+            </ClientOnly>
+            <div id="main-container" class="container bg-light z-10 px-14 py-10"> 
                 <slot />
             </div>
         </div>
@@ -20,5 +29,33 @@
 
 <script setup lang="ts">
     const settings = useSettings();
-    const bgImg = settings.value?.data.background[0];
+
+    const bgImgUrls = settings.value?.data.background?.map(image => image.image.url);
+
+    // Images size is set in Prismic: 196*277
+    const imgWidth = 196;
+    const imgHeight = 277;
+
+    const cols = ref(0);
+    const rows = ref(0);
+
+    const updateGrid = () => {
+        const element = window. document.getElementById('main-container');
+        console.log(element?.clientHeight)
+
+        cols.value = Math.ceil(window.innerWidth / imgWidth);
+        rows.value = Math.ceil((element ? element.clientHeight : window.innerHeight) / imgHeight);
+    };
+
+    onMounted(() => {
+        updateGrid();
+        window.addEventListener('resize', updateGrid);
+    });
+
+    const totalTiles = computed(() => cols.value * rows.value);
+
+    const gridStyle = computed(() => ({
+        gridTemplateColumns: `repeat(${cols.value}, ${imgWidth}px)`,
+        gridAutoRows: `${imgHeight}px`,
+    }));
 </script>
