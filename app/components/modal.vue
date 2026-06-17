f2f054b9f2a08e181649d57376846ec8c6ea7237
SamMa7 <sam.maass1998@gmail.com>
2026-06-17 11:54:46 +0200

    Gallery (+modal)

---
+++ app/components/modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <Transition>
+        <div v-if="showModal" class="fixed top-0 left-0 right-0 bottom-0 size-full">
+            <div class="overlay-dark" @click="showModal = false"/>
+            <div class="bg-white z-50 fixed top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 w-3/6 h-5/6 rounded-md">
+                <button 
+                    class="btn-link fixed right-5 top-5"  
+                    @click="showModal = false"
+                > 
+                    <Icon name="mdi:close" size="25"/> 
+                </button>
+                <slot></slot>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script setup lang="ts">
+    const showModal = defineModel('showModal', {default: false, required: true});
+</script>
+
--- app/slices/Gallery/index.vue
+++ app/slices/Gallery/index.vue
@@ -1,20 +1,66 @@
-<script setup lang="ts">
-  import type { Content } from "@prismicio/client";
-
-  defineProps(getSliceComponentProps<Content.GallerySlice>());
-
-
-</script>
-
 <template>
   <section
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
     <div class="columns-1 gap-3 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-      <NuxtImg v-for="picture in slice.primary.pictures" class="w-full h-auto" loading="lazy" :alt="picture.picture.alt" :src="picture.picture.url ?? undefined"/>
+      <NuxtImg 
+        v-for="(picture, _index) in slice.primary.pictures" 
+        class="w-full h-auto" 
+        loading="lazy" 
+        :alt="picture.picture.alt" 
+        :src="picture.picture.url ?? undefined"
+        @click="openPictureModal(_index)"
+      />
     </div>
+
+    <Modal v-model:show-modal="open">
+      <div class="p-10 flex size-full justify-between">
+        <button 
+          class="btn-dark rounded-full size-12 self-center"
+          :disabled="currentIndex === 0"
+          @click="prevImage()"
+        >
+          <Icon name="mdi:arrow-left" size="25"/>
+        </button>
+        <NuxtImg 
+          class="object-contain max-w-[80%]" 
+          loading="lazy" 
+          :alt="slice.primary.pictures[currentIndex]?.picture.alt" 
+          :src="slice.primary.pictures[currentIndex]?.picture.url ?? undefined"
+        />
+        <button 
+          class="btn-dark rounded-full size-12 self-center"
+          :disabled="currentIndex === slice.primary.pictures.length-1"
+          @click="nextImage()"
+        >
+          <Icon name="mdi:arrow-right" size="25"/>
+        </button>
+      </div>
+    </Modal>
   </section>
 </template>
 
+<script setup lang="ts">
+  import type { Content } from "@prismicio/client";
+  import Modal from "~/components/modal.vue";
 
+  defineProps(getSliceComponentProps<Content.GallerySlice>());
+
+  const open = ref(false);
+  const currentIndex = ref(0)
+
+  const openPictureModal = (index: number) => {
+    if(!index && index !== 0) return;
+
+    open.value = true;
+    currentIndex.value = index
+  }
+
+  const nextImage = () => {
+    currentIndex.value++;
+  }
+  const prevImage = () => {
+    currentIndex.value--;
+  }
+</script>
