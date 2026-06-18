1e74275dc04626caa8a43df7c8ce3f5461c88932
SamMa7 <sam.maass1998@gmail.com>
2026-06-18 11:22:41 +0200

    reworked url

--- app/util/helperFunctions.ts
+++ app/util/helperFunctions.ts
@@ -3,6 +3,6 @@ import type { LinkField } from "@prismicio/client";
 
 // HELPER FUNCTIONS
 export const getUrl = (link: LinkField) => {
-    if(link.link_type === 'Document') return `/${link.uid}`;
     if(link.link_type === 'Web') return link.url;
+    if(link.link_type === 'Document') return `/${link.type === "plain_text" ? link.uid : link.slug}`;
 };
