128ca45cc4d0849fbc827cb57447343e9885687e
SamMa7 <sam.maass1998@gmail.com>
2026-06-24 14:49:37 +0200

    comic mobile

--- app/slices/Comic/index.vue
+++ app/slices/Comic/index.vue
@@ -3,13 +3,13 @@
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    <div class="grid grid-cols-3 w-full">
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 w-full justify-center gap-y-10">
       <div v-for="comic in slice.primary.comics" @click="openComicModal(comic)">
         <h2 class="pb-8">{{ comic.title }}</h2>
         <div class="relative group hover:z-50">
           <NuxtImg 
             v-if="comic.slides[2]"
-            class="object-contain h-[250px] z-10 absolute top-0 transition-all group-hover:scale-110 group-hover:origin-bottom group-hover:rotate-[24deg]" 
+            class="object-contain h-[250px] z-10 top-0 transition-all group-hover:scale-110 group-hover:origin-bottom group-hover:rotate-[24deg]" 
             loading="lazy" 
             :src="getUrl(comic.slides[2])"
           />
@@ -30,9 +30,9 @@
     </div>
 
     <Modal v-model:show-modal="open">
-      <div class="p-10 flex size-full justify-center items-center">
+      <div class="p-2 sm:p-10 flex size-full justify-center items-center z-40">
         <button 
-            class="btn-link-light fixed right-5 top-5"  
+            class="btn-link-light fixed right-5 top-5 z-50"  
             @click="open = false"
         > 
             <Icon name="mdi:close" size="30"/> 
@@ -46,14 +46,14 @@
             :pages="currentComicPages" 
           > 
             <button 
-              class="btn-dark rounded-full size-12 z-50 fixed left-32 top-1/2"
+              class="btn-dark rounded-full size-12 z-50 fixed left-5 bottom-20 lg:left-32 lg:top-1/2"
               :disabled="!flipBookRef.canFlipLeft"
               @click="flipBookRef.flipLeft"
             >
               <Icon name="mdi:arrow-left" size="25"/>
             </button>
             <button 
-              class="btn-dark rounded-full size-12 z-50 fixed right-32 top-1/2"
+              class="btn-dark rounded-full size-12 z-50 fixed  right-5 bottom-20 lg:right-32 lg:top-1/2"
               :disabled="!flipBookRef.canFlipRight"
               @click="flipBookRef.flipRight"
             >
